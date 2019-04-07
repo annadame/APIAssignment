09b3ce6e5d9d2a8c4c211e892a57c34e80968568
annadame <ajdame@avans.nl>
2019-04-07 13:26:00 +0200

    Fizzbuzz works

---
+++ IndiciaApp/IndiciaApp/Controllers/FizzbuzzController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IndiciaApp.Controllers
+{
+    public class FizzbuzzController : ApiController
+    {
+        //[Route("Fizzbuzz/{from}/{to}")]
+        public IEnumerable<string> Get([FromUri] int from, int to)
+        {
+            int number = to;
+            List<string> output = new List<string>();
+
+            for (int i = from; i <= number; i++)
+            {
+                List<string> fizzbuzzvalues = new List<string>();
+
+                if (i % 3 == 0)
+                {
+                    fizzbuzzvalues.Add("Fizz");
+                }
+
+                if (i % 5 == 0)
+                {
+                    fizzbuzzvalues.Add("Buzz");
+                }
+
+                if (fizzbuzzvalues.Count <= 0)
+                {
+                    output.Add(i.ToString());
+                }
+                else
+                {
+                    string values = String.Join("", fizzbuzzvalues.ToArray());
+                    output.Add(values);
+                }
+            }
+
+            return output;
+        }
+    }
+}
